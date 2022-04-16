23db97f8cda25e8bb66b17ea85ef0c09aac8d302
Anukit Sornboonta <anukit084@hotmail.com>
2022-04-16 14:48:32 +0700

    update

--- lib/modules/constant.dart
+++ lib/modules/constant.dart
@@ -1,5 +1,4 @@
 class Constant {
-  // String domain = 'http://172.20.125.93:5000'; //url localhost
-  String domain = 'http://128.199.96.80:5000'; //url Server
+  String domain = 'http://172.20.125.93:5000'; //url localhost
   Constant();
 }
